434463685ab98f5f671837780c6fa2db45c15f2b
SlavovaDaria <slavova19@gmail.com>
2022-05-07 23:21:59 +0300

    Switch to upper case

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello, world!");
+        System.out.println("Hello, World!");
         System.out.println("Hello, QA GURU");
-        System.out.println("A kto takie fixiki - bolshoi bolshoi sekret!");
+        System.out.println("A Kto Takie Fixiki - Bolshoi Bolshoi Sekret!");
     }
 }
