3e7efcdca2501610839f5432c2574649df57f9e6
SlavovaDaria <slavova19@gmail.com>
2022-05-07 20:55:08 +0300

    commas and new line added

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-    }
-    static {
-        System.out.println("Hello, QA Guru");
+        System.out.println("Hello, world!");
+        System.out.println("Hello, QA GURU");
+        System.out.println("A kto takie fixiki - bolshoi bolshoi sekret!");
     }
 }
