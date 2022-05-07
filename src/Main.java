324ad50c295521e183acb2fb1382f803ba8c687d
SlavovaDaria <slavova19@gmail.com>
2022-05-07 20:11:24 +0300

    Added new line

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-    }
-    static {
         System.out.println("Hello, QA Guru");
     }
 }
